54593e1d7cfa4050aae5062cca643588373991fa
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-01-05 13:36:39 +0530

    react parts

--- reactstart/src/App.jsx
+++ reactstart/src/App.jsx
@@ -32,4 +32,15 @@ import './App.css'
 //   )
 // }
 
+
+function App(){
+    return(
+        <div className="App">
+            <h1>Helloo World</h1>
+        </div>
+    );
+}
+
+export default App;
+
 // export default App
