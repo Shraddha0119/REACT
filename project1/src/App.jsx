61e9eceb3b64ca2689bc7ae2da3a16511a686bfe
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-01-12 17:15:27 +0530

    react start

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,14 +1,13 @@
 
 
 
-import comp1 from "./components";
 
 function App() {
 
 
     return(
         <div className="App">
-            {comp1()}
+       
         </div>
     );
 }
--- project1/src/Components.jsx
+++
@@ -1,6 +0,0 @@
-function comp1(){
-    console.log("hello");
-    
-}
-
-export default comp1
