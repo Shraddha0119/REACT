de1975f175ba0f9214c095943b9bc7eb8a675c30
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-02-27 15:47:12 +0530

    practice

--- react.practice/src/App.jsx
+++ react.practice/src/App.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 
 function App() {
 
-    const languages = ['JS', 'Python', 'Java'];
-    const morelanguages = ['C', 'C++', 'C#']
-
-    const allLanguages = [...languages, ...morelanguages]
+  
 
 
   return (
