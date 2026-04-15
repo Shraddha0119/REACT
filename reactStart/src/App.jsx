a1fdb5ceca71d8f6e6bc040a37c1ed9c2743aa3c
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-04-15 16:32:03 +0530

    practice

---
+++ reactStart/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Navbar/>
+    <div>
+     
+    <h1>hello i am Web Developer</h1>
+    <div  className='count'>{count}</div>
+    <button className='btn'  onClick={()=>{setCount(count+1)}}>Click</button>
+    </div>
+    </>
+  )
+}
+
+export default App
