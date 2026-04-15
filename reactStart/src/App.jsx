095dd78dabd871b073a8121d610cd6bc8227f133
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-04-15 18:28:51 +0530

    practice

--- reactStart/src/App.jsx
+++ reactStart/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState ,useEffect } from 'react'
 
 import './App.css'
 import Navbar from './components/Navbar'
@@ -6,15 +6,28 @@ import Navbar from './components/Navbar'
 function App() {
   const [count, setCount] = useState(0)
 
+
+  useEffect(() => {
+    alert("hey welcome to mypage")
+  
+  },[])
+  useEffect(() => {
+    alert("count was change ")
+  
+  },[count])
+  
+
   return (
     <>
-     <Navbar/>
+     {/* <Navbar/> */}
     {/* <div>
      
     <h1>hello i am Web Developer</h1>
     <div  className='count'>{count}</div>
     <button className='btn'  onClick={()=>{setCount(count+1)}}>Click</button>
     </div> */}
+    <div  className='count'>{count}</div>
+     <button className='btn'  onClick={()=>{setCount(count+1)}}>Click</button>
     </>
   )
 }
