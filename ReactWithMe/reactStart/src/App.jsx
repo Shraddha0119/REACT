9c3c07be6d6b2ea9bc8e7c8389c8ec11a46c8460
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-04-25 11:35:10 +0530

    practice

---
+++ ReactWithMe/reactStart/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState ,useEffect } from 'react'
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  useEffect(() => {
+    alert("hey welcome to mypage")
+  
+  },[])
+  useEffect(() => {
+    alert("count was change ")
+  
+  },[count])
+  
+
+  return (
+    <>
+     {/* <Navbar/> */}
+    {/* <div>
+     
+    <h1>hello i am Web Developer</h1>
+    <div  className='count'>{count}</div>
+    <button className='btn'  onClick={()=>{setCount(count+1)}}>Click</button>
+    </div> */}
+    
+    <div  className='count'>{count}</div>
+     <button className='btn'  onClick={()=>{setCount(count+1)}}>Click</button>
+    </>
+  )
+}
+
+export default App
---
+++ ReactWithMe/reactStart/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <h1>home</h1>
+        <h1>cart</h1>
+        <h1>login</h1>
+        <h1>signup</h1>
+    </div>
+  )
+}
+
+export default Navbar
